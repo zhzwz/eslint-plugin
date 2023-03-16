bc45213352f5c52bccafacd6f321a007e2d0e69a
zhzwz <zhaoziweizhi@gmail.com>
2023-03-16 15:37:30 +0800

    chore: change config files name

--- source/configs/json-vscode-settings.ts
+++ source/configs/json-vscode-settings.ts
@@ -1,13 +1,13 @@
 export default {
-  plugins: [
-    'eslint-plugin-jsonc',
-  ],
+  plugins: ['eslint-plugin-jsonc'],
   overrides: [
     {
       files: ['**/.vscode/settings.json'],
       parser: 'jsonc-eslint-parser',
       rules: {
+        // 规则按首字母排序
         'jsonc/sort-keys': ['error', 'asc'],
+        // 数组值按首字母排序
         'jsonc/sort-array-values': ['error', { pathPattern: '.*', order: { type: 'asc' } }],
       },
     },

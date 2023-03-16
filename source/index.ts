02a37d5ca19dbbf58604fd7c8e9b8a1b8c6b8ddf
zhzwz <zhaoziweizhi@gmail.com>
2023-03-16 15:43:52 +0800

    build: version 0.1.0

--- source/index.ts
+++ source/index.ts
@@ -1,36 +1,23 @@
-import jsonConfig from './configs/json'
-import standardConfig from '././configs/standard'
-
-import yamlConfig from './configs/yaml'
-import markdownConfig from './configs/markdown'
-import vueConfig from './configs/vue'
-import reactConfig from './configs/react'
-
-import packageJsonConfig from './configs/package-json'
-import vscodeSettingsJsonConfig from './configs/vscode-settings-json'
+import standard from '././configs/standard'
+import vue from './configs/vue'
+import react from './configs/react'
+import json from './configs/json'
+import jsonPackage from './configs/json-package'
+import jsonVscodeSettings from './configs/json-vscode-settings'
+import yaml from './configs/yaml'
+import markdown from './configs/markdown'
+import unocss from './configs/unocss'
 
 export default {
   configs: {
-    standard: standardConfig,
-    vue: vueConfig,
-    react: reactConfig,
-    yaml: yamlConfig,
-    markdown: markdownConfig,
-    json: jsonConfig,
-    'package-json': packageJsonConfig,
-    'vscode-settings-json': vscodeSettingsJsonConfig,
-    all: {
-      extends: [
-        'plugin:@zhzwz/react',
-        'plugin:@zhzwz/vue',
-
-        'plugin:@zhzwz/json',
-        'plugin:@zhzwz/package-json',
-        'plugin:@zhzwz/yaml',
-        'plugin:@zhzwz/markdown',
-
-        'plugin:@zhzwz/vscode-settings-json',
-      ],
-    },
+    standard,
+    vue,
+    react,
+    json,
+    'json-package': jsonPackage,
+    'json-vscode-settings': jsonVscodeSettings,
+    yaml,
+    markdown,
+    unocss,
   },
 }

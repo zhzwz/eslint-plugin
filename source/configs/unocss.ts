ff84d388c5857ebce91c97ceaa2dc7807e66c05e
zhzwz <zhaoziweizhi@gmail.com>
2023-03-16 15:42:23 +0800

    feat: add unocss support

---
+++ source/configs/unocss.ts
@@ -0,0 +1,9 @@
+// https://github.com/unocss/unocss/tree/main/packages/eslint-config
+
+export default {
+  plugins: ['@unocss'],
+  rules: {
+    '@unocss/order': 'warn',
+    '@unocss/order-attributify': 'warn',
+  },
+}
